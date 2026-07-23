372739a6153d1f999e797ac5bd1c37cf3ca805d7
Sanjanabagul <sanjanabagul85@gmail.com>
2026-07-23 14:14:58 +0530

    Initial commit

---
+++ class2/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <div className='h-125 w-100% bg-black text-white'>
+      <Nav/>
+      <h1 className='text-5xl bg-black text-cyan-200 font-bold'>Suno, mere veero!</h1>
+    </div>
+  )
+}
+
+export default App
