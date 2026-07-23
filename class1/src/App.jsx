5e9037453d424067bfd08cc5ac6ed91d7b509954
Sanjanabagul <sanjanabagul85@gmail.com>
2026-07-23 10:28:34 +0530

    new folder added

---
+++ class1/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import Section from './components/Section'
+import Center from './components/Center'
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <Section/>
+      {/* <Center/> */}
+    <Footer/>
+    </div>
+  )
+}
+
+export default App
